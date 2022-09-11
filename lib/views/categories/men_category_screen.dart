9752cadb51292c16301912e8b548779ca4c1980b
mbuguap <petrmbugua@gmail.com>
2022-09-11 13:17:17 +0300

    sub categroy

--- lib/views/categories/men_category_screen.dart
+++ lib/views/categories/men_category_screen.dart
@@ -1,16 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:multi_vendor/utils/category_list.dart';
+import 'package:multi_vendor/views/minor_screens/sub_category_screen.dart';
 
 class MenCategoryScreen extends StatelessWidget {
-  List<String> men = [
-    'Shirt',
-    'T-Shirt',
-    'Jacket',
-    'Vest',
-    'Coat',
-    'Jeans',
-    'Shorts',
-    'Suit'
-  ];
+ 
 
   @override
   Widget build(BuildContext context) {
@@ -30,21 +23,32 @@ class MenCategoryScreen extends StatelessWidget {
             crossAxisCount: 3,
             crossAxisSpacing: 15,
             mainAxisSpacing: 70,
-            children: List.generate(8, (index) {
+            children: List.generate(men.length, (index) {
               return Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: [
-                    SizedBox(
-                      // color: Colors.cyan,
-                      height: 50,
-                      width: 50,
-                      child: Image.asset('assets/images/men/men$index.jpg'),
-                    ),
-                    Text(
-                      men[index],
-                    ),
-                  ],
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(
+                        MaterialPageRoute(builder: (BuildContext context) {
+                      return SubCategoryScreen(
+                        subCategoryName: men[index],
+                        mainCategory: 'Men',
+                      );
+                    }));
+                  },
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        // color: Colors.cyan,
+                        height: 50,
+                        width: 50,
+                        child: Image.asset('assets/images/men/men$index.jpg'),
+                      ),
+                      Text(
+                        men[index],
+                      ),
+                    ],
+                  ),
                 ),
               );
             }),
