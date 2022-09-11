5ee6ac2afb6d784c32c0a937b07c2213cc9dc01f
mbuguap <petrmbugua@gmail.com>
2022-09-11 10:48:58 +0300

    tabs

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -6,57 +6,102 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: InkWell(
-          onTap: () {
-            Navigator.of(context)
-                .push(MaterialPageRoute(builder: (BuildContext context) {
-              return SearchScreen();
-            }));
-          },
-          child: Container(
-            height: 40,
-            decoration: BoxDecoration(
-              border: Border.all(width: 1.6, color: Colors.cyan),
-              borderRadius: BorderRadius.circular(
-                25,
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: InkWell(
+            onTap: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (BuildContext context) {
+                return SearchScreen();
+              }));
+            },
+            child: Container(
+              height: 40,
+              decoration: BoxDecoration(
+                border: Border.all(width: 1.6, color: Colors.cyan),
+                borderRadius: BorderRadius.circular(
+                  25,
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(
+                    Icons.search,
+                    color: Colors.grey,
+                    size: 30,
+                  ),
+                  Text(
+                    'What are you looking for ...',
+                    style: TextStyle(color: Colors.grey, fontSize: 18),
+                  ),
+                  Container(
+                    height: 33,
+                    width: 74,
+                    decoration: BoxDecoration(
+                      color: Colors.cyan,
+                      borderRadius: BorderRadius.circular(
+                        25,
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Search',
+                        style: TextStyle(color: Colors.white, fontSize: 15),
+                      ),
+                    ),
+                  )
+                ],
               ),
             ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Icon(
-                  Icons.search,
-                  color: Colors.grey,
-                  size: 30,
-                ),
-                Text(
-                  'What are you looking for ...',
-                  style: TextStyle(color: Colors.grey, fontSize: 18),
-                ),
-                Container(
-                  height: 33,
-                  width: 74,
-                  decoration: BoxDecoration(
-                    color: Colors.cyan,
-                    borderRadius: BorderRadius.circular(
-                      25,
-                    ),
-                  ),
-                  child: Center(
-                    child: Text(
-                      'Search',
-                      style: TextStyle(color: Colors.white, fontSize: 15),
-                    ),
-                  ),
-                )
-              ],
-            ),
+          ),
+          elevation: 0,
+          bottom: TabBar(
+            tabs: [
+              RepeatedTab(title: 'Men',),
+              RepeatedTab(title: 'Women',),
+              RepeatedTab(title: 'Kids',),
+              RepeatedTab(title: 'Shoes',),
+            ],
           ),
         ),
-        elevation: 0,
+        body: TabBarView(
+          children: [
+            Center(
+              child: Text('Men Category'),
+            ),
+            Center(
+              child: Text('Women Category'),
+            ),
+            Center(
+              child: Text('Kids Category'),
+            ),
+            Center(
+              child: Text('Shoes Category'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RepeatedTab extends StatelessWidget {
+  final String title;
+  const RepeatedTab({
+    required this.title,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Tab(
+      child: Text(
+        title,
+        style: TextStyle(color: Colors.grey.shade600),
       ),
     );
   }
