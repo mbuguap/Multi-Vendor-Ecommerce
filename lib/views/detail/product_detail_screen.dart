1b6f2d51039afdd1af4e06fdb5e97eac541f446f
mbuguap <petrmbugua@gmail.com>
2022-09-15 18:56:53 +0300

    bottom sheet

--- lib/views/detail/product_detail_screen.dart
+++ lib/views/detail/product_detail_screen.dart
@@ -1,7 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_swiper_null_safety/flutter_swiper_null_safety.dart';
+import 'package:multi_vendor/views/widgets/product_model.dart';
+import 'package:staggered_grid_view_flutter/widgets/staggered_grid_view.dart';
+import 'package:staggered_grid_view_flutter/widgets/staggered_tile.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   final dynamic productList;
@@ -10,21 +14,217 @@ class ProductDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<dynamic> images = productList['productImage'];
+    final Stream<QuerySnapshot> _productsStream = FirebaseFirestore.instance
+        .collection('products')
+        .where('mainCategory', isEqualTo: 'women')
+        .snapshots();
     return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.45,
-            child: Swiper(
-              itemCount: 1,
-              pagination: SwiperPagination(builder: SwiperPagination.dots),
-              itemBuilder: (context, index) {
-                return Image.network(
-                    'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png');
-              },
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.45,
+              child: Swiper(
+                itemCount: images.length,
+                pagination:
+                    SwiperPagination(builder: SwiperPagination.fraction),
+                itemBuilder: (context, index) {
+                  return Image.network(images[index]);
+                },
+              ),
             ),
-          ),
-        ],
+            Text(
+              productList['productName'],
+              style: TextStyle(
+                  fontSize: 19,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 4,
+                  color: Colors.grey.shade600),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        'USD',
+                        style: TextStyle(fontSize: 18, color: Colors.cyan),
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        productList['price'].toString(),
+                        style: TextStyle(fontSize: 18, color: Colors.cyan),
+                      ),
+                    ],
+                  ),
+                  IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.favorite_border_outlined,
+                        color: Colors.red,
+                        size: 30,
+                      ))
+                ],
+              ),
+            ),
+            Text(
+              '${productList['instock']} pieces available in stock',
+              style: TextStyle(color: Colors.grey),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 40,
+                  width: 50,
+                  child: Divider(
+                    color: Colors.grey,
+                    thickness: 1,
+                  ),
+                ),
+                Text(
+                  ' Item Description ',
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.cyan),
+                ),
+                SizedBox(
+                  height: 40,
+                  width: 50,
+                  child: Divider(
+                    color: Colors.grey,
+                    thickness: 1,
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text(
+                productList['productDescription'],
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.grey),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 40,
+                  width: 50,
+                  child: Divider(
+                    color: Colors.grey,
+                    thickness: 1,
+                  ),
+                ),
+                Text(
+                  ' Similar Items ',
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.cyan),
+                ),
+                SizedBox(
+                  height: 40,
+                  width: 50,
+                  child: Divider(
+                    color: Colors.grey,
+                    thickness: 1,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              child: StreamBuilder<QuerySnapshot>(
+                stream: _productsStream,
+                builder: (BuildContext context,
+                    AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.hasError) {
+                    return Text('Something went wrong');
+                  }
+
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(
+                      child: CircularProgressIndicator(
+                        color: Colors.cyan,
+                      ),
+                    );
+                  }
+
+                  if (snapshot.data!.docs.isEmpty) {
+                    return Center(
+                      child: Text(
+                        'No Items in this category',
+                        style: TextStyle(
+                            color: Colors.blueGrey,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    );
+                  }
+
+                  return SingleChildScrollView(
+                    child: StaggeredGridView.countBuilder(
+                        shrinkWrap: true,
+                        itemCount: snapshot.data!.docs.length,
+                        crossAxisCount: 2,
+                        itemBuilder: (BuildContext context, int index) {
+                          return ProductModel(
+                            products: snapshot.data!.docs[index],
+                          );
+                        },
+                        staggeredTileBuilder: (context) =>
+                            StaggeredTile.fit(1)),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomSheet: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.store),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.shopping_cart),
+            ),
+            Container(
+              height: 35,
+              width: MediaQuery.of(context).size.width * 0.45,
+              decoration: BoxDecoration(
+                color: Colors.cyan,
+                borderRadius: BorderRadius.circular(
+                  15,
+                ),
+              ),
+              child: MaterialButton(
+                onPressed: () {},
+                child: Text(
+                  "ADD TO CART",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 17,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
